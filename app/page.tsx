ed8de90f9840ac5d96b0f56d50e8da8cb6814738
Nam <namdeveloper.ca@gmail.com>
2023-05-06 11:51:40 +0700

    dynamic fetching data

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
-import Link from 'next/link'
-export default function Home() {
+import NewsCard from '@/components/NewsCard'
+import { NewsResponse } from '@/models/NewsArticles'
+
+async function getArticle(): Promise<NewsResponse> {
+  // Dynamic Data Fetching
+  const res = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=${process.env.COUNTRY}&apiKey=${process.env.NEWS_API_KEY}`,
+    { cache: 'no-store' }
+  )
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function BreakingNews() {
+  const data = await getArticle()
+
   return (
-    <main>
-      <ul>
-        <li>
-          <Link href='/search'>Search</Link>
-          <Link href='/categories/tech'>Tech</Link>
-        </li>
-      </ul>
+    <main className='container mx-auto'>
+      <h1 className='text-5xl my-6'>Breaking News</h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+        {data.articles.map((article, index) => (
+          <div key={index}>
+            <NewsCard article={article} />
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
